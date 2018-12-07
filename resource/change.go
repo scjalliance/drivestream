a999549965911e015daa24a62dcd8d40b129acb0
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-12-06 17:48:56 -0800

    resource: Leaner JSON marshaling of changes

--- resource/change.go
+++ resource/change.go
@@ -1,6 +1,8 @@
 package resource
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -11,4 +13,36 @@ type Change struct {
 	Removed bool      `json:"removed,omitempty"`
 	File    `json:"file,omitempty"`
 	Drive   `json:"drive,omitempty"`
+}
+
+// MarshalJSON marshals the change as JSON-encoded bytes.
+func (c Change) MarshalJSON() ([]byte, error) {
+	switch c.Type {
+	case TypeFile:
+		return json.Marshal(struct {
+			Type    Type      `json:"type"`
+			Time    time.Time `json:"time"`
+			Removed bool      `json:"removed,omitempty"`
+			File    `json:"file"`
+		}{
+			Type:    c.Type,
+			Time:    c.Time,
+			Removed: c.Removed,
+			File:    c.File,
+		})
+	case TypeDrive:
+		return json.Marshal(struct {
+			Type    Type      `json:"type"`
+			Time    time.Time `json:"time"`
+			Removed bool      `json:"removed,omitempty"`
+			Drive   `json:"drive"`
+		}{
+			Type:    c.Type,
+			Time:    c.Time,
+			Removed: c.Removed,
+			Drive:   c.Drive,
+		})
+	default:
+		return nil, fmt.Errorf("unknown resource type \"%s\"", c.Type)
+	}
 }
